65549703e03ccc5708082d2007d66a7f05329630
Benjamin Adams <ben.adams@rpsgroup.com>
2023-05-24 11:54:48 -0400

    Update maintenance scripts

--- contrib/scripts/clear-harvests.sh
+++ contrib/scripts/clear-harvests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-HARVEST="$CKAN_HOME/bin/paster --plugin=ckanext-harvest harvester -c $CKAN_CONFIG/ckan.ini"
+HARVEST="ckan -c /etc/ckan/production.ini harvester"
 for source in $($HARVEST sources | grep 'Source id' | awk '{print $3}'); do
-    $HARVEST clearsource $source
+    $HARVEST clearsource "$source"
 done
 
 $HARVEST job-all
--- contrib/scripts/clear_stuck_harvests.bash
+++ contrib/scripts/clear_stuck_harvests.bash
@@ -3,8 +3,7 @@
 # Delete any harvest jobs
 
 while IFS= read -r id; do
-    ckan-paster --plugin=ckanext-harvest harvester \
-         -c /etc/ckan/production.ini job_abort "$id";
+    ckan -c /etc/ckan/production.ini harvester job-abort "$id";
 done < <(psql -h "${DB_HOST:-db}" -U ckan -qAt -c "SELECT id FROM harvest_job WHERE status = 'Running' AND gather_started <= NOW() - '3 day'::interval")
 
-ckan-paster --plugin=ckanext-harvest harvester -c /etc/ckan/production.ini job-all
+ckan -c /etc/ckan/production.ini harvester job-all
