569d271d52791657adb9a779ddd7c4f484effe02
Benjamin Adams <benjamin.adams@tetratech.com>
2024-03-04 14:50:12 -0500

    Remove job-all/harvest re-run after clearing stuck harvests

--- contrib/scripts/clear_stuck_harvests.bash
+++ contrib/scripts/clear_stuck_harvests.bash
@@ -5,5 +5,3 @@
 while IFS= read -r id; do
     ckan -c /etc/ckan/production.ini harvester job-abort "$id";
 done < <(psql -h "${DB_HOST:-db}" -U ckan -qAt -c "SELECT id FROM harvest_job WHERE status = 'Running' AND created <= NOW() - '3 day'::interval")
-
-ckan -c /etc/ckan/production.ini harvester job-all
