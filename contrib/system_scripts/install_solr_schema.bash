d0b04248a1ffcf32059c0918f631269a45f1b460
Benjamin Adams <benjamin.adams@tetratech.com>
2025-03-26 14:51:41 -0400

    Treat "Gulf of America" and "Gulf of Mexico" as synonyms
    Adds synonym mapping between "Gulf of America" and "Gulf of Mexico".
    Update Solr schema loading script accordingly to accomodate these
    changes.  Also changes GCOOS references from "Gulf of Mexico Coastal
    Ocean Observing System" to "Gulf of America Coastal Ocean Observing
    System" and changes to a new logo in organization loading scripts.

--- contrib/system_scripts/install_solr_schema.bash
+++ contrib/system_scripts/install_solr_schema.bash
@@ -11,9 +11,10 @@ else
 fi
 solr_container_name=${1:-solr}
 
-# 1) Copy over changes to managed-schema file
+# 1) Copy over changes to managed-schema file and create synonyms file
 
 docker cp "$source_dir/../solr/schema.xml" "$solr_container_name":/var/solr/data/ckan/conf/managed-schema
+docker cp "$source_dir/../solr/synonyms.txt" "$solr_container_name":/var/solr/data/ckan/conf/synonyms.txt
 
 # 2) Copy managed-schema to schema.xml
 docker exec "$solr_container_name" cp /var/solr/data/ckan/conf/managed-schema /var/solr/data/ckan/conf/schema.xml
@@ -22,16 +23,15 @@ docker exec "$solr_container_name" cp /var/solr/data/ckan/conf/managed-schema /v
 #docker exec "$solr_container_name" sed -i '/^updateProcessor/i\  <schemaFactory class="ClassicIndexSchemaFactory"\/>' /var/solr/data/ckan/conf/solrconfig.xml
 docker exec "$solr_container_name" sed -i "0,/^[[:space:]]*<updateProcessor/s@@<schemaFactory class=\"ClassicIndexSchemaFactory\"/>\n\0@" /var/solr/data/ckan/conf/solrconfig.xml
 
-# 4) Restart solr
-docker restart "$solr_container_name"
+# 4) Reload CKAN Solr core
+curl "http://localhost:8983/solr/admin/cores?action=RELOAD&core=ckan"
 
 # 5) Remove ClassicIndexSchemaFactory
 docker exec "$solr_container_name" sed -i '/<schemaFactory class="ClassicIndexSchemaFactory"\/>/d' /var/solr/data/ckan/conf/solrconfig.xml
 
-# 6) restart solr again
-docker restart "$solr_container_name"
+# 6) Reload CKAN Solr core once again
+curl "http://localhost:8983/solr/admin/cores?action=RELOAD&core=ckan"
 
 # 7) rebuild index
-# needs to wait
-# TODO: Find out way to determine when Solr is ready
-echo 'Schema move complete. Run `docker exec ckan ckan -c /srv/app/production.ini search-index rebuild` once solr is ready'
+echo 'Rebuilding Solr index:'
+docker exec ckan ckan -c /srv/app/production.ini search-index rebuild
